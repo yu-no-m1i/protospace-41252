92d3b1cf28f28d750ea2fd9f844d22a05ceaf09b
yu-no-m1i <snowapple.2525@gmail.com>
2024-07-23 14:24:57 +0900

    プロトスペースの設定

--- Gemfile
+++ Gemfile
@@ -66,7 +66,10 @@ end
 
 group :test do
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
+
   gem "capybara"
   gem "selenium-webdriver"
   gem "webdrivers"
+
 end
+gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'prototypes/index'
   get 'messages/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-    root to: "messages#index"
+    root to: "prototypes#index"
 end
